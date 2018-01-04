4bcda71aaf14fbec5a6cd02c5a6c3b4b60509eb0
libraiger <misssilverbeauty0927@outlook.com>
2018-01-04 15:54:18 +0000

    Agent deceleration (WIP)

--- unity_workspace/Assets/Scripts/Controller/AgentController.cs
+++ unity_workspace/Assets/Scripts/Controller/AgentController.cs
@@ -11,12 +11,18 @@ public class AgentController : MonoBehaviour
 	private float m_moveBackwardSpeed = 7.0f;
 	[SerializeField]
 	private float m_moveSidewaysSpeed = 6.0f;
+
 	private Vector3 m_movementStep = Vector3.zero;
+	private Vector3 m_lastMovementStep = Vector3.zero;
+	private Vector3 m_appliedMovementStep = Vector3.zero;
 
 	// rotation
 	[SerializeField]
-	private float m_rotateSpeed = 360.0f;
+	private float m_rotationSpeed = 360.0f;
+
 	private float m_rotationStep = 0.0f;
+	private float m_lastRotationStep = 0.0f;
+	private float m_appliedRotationStep = 0.0f;
 
 	// movement acceleration, deceleration
 	[SerializeField]
@@ -58,20 +64,20 @@ public class AgentController : MonoBehaviour
 				if (m_movementAcceleration < 1.0f)
 				{
 					m_movementAcceleration = Mathf.Clamp(m_movementAcceleration + Time.deltaTime * m_movementAccelerationFactor, 0.0f, 1.0f);
+					m_appliedMovementStep = m_movementStep * m_movementAcceleration;
 				}
 			}
 			else
 			{
-				// #SteveD >>> Deceleration is immediate as m_movementStep is reset every frame. Requires a cached version to be used
-				// when no input is received
 				if (m_movementAcceleration > 0.0f)
 				{
 					m_movementAcceleration = Mathf.Clamp(m_movementAcceleration - Time.deltaTime * m_movementDecelerationFactor, 0.0f, 1.0f);
+					m_appliedMovementStep = m_lastMovementStep * m_movementAcceleration;
 				}
 			}
 
 			// move
-			m_characterController.Move(m_movementStep * m_movementAcceleration);
+			m_characterController.Move(m_appliedMovementStep);
 			m_movementStep.Set(0.0f, 0.0f, 0.0f);
 
 			// rotation acceleration
@@ -80,20 +86,20 @@ public class AgentController : MonoBehaviour
 				if (m_rotationAcceleration < 1.0f)
 				{
 					m_rotationAcceleration = Mathf.Clamp(m_rotationAcceleration + Time.deltaTime * m_rotationAccelerationFactor, 0.0f, 1.0f);
+					m_appliedRotationStep = m_rotationStep * m_rotationAcceleration;
 				}
 			}
 			else
 			{
-				// #SteveD >>> Deceleration is immediate as m_rotationStep is reset every frame. Requires a cached version to be used
-				// when no input is received
 				if (m_rotationAcceleration > 0.0f)
 				{
 					m_rotationAcceleration = Mathf.Clamp(m_rotationAcceleration - Time.deltaTime * m_rotationDecelerationFactor, 0.0f, 1.0f);
+					m_appliedRotationStep = m_lastRotationStep * m_rotationAcceleration;
 				}
 			}
 
 			// rotate
-			m_transform.Rotate(0.0f, m_rotationStep * m_rotationAcceleration, 0.0f);
+			m_transform.Rotate(0.0f, m_appliedRotationStep, 0.0f);
 			m_rotationStep = 0.0f;
 		}
 	}
@@ -110,6 +116,7 @@ public class AgentController : MonoBehaviour
 		{
 			m_movementStep += m_transform.forward * value * m_moveBackwardSpeed * Time.deltaTime;
 		}
+		m_lastMovementStep = m_movementStep;
 	}
 
 	// --------------------------------------------------------------------------------
@@ -117,13 +124,15 @@ public class AgentController : MonoBehaviour
 	public void MoveSideways(float value)
 	{
 		m_movementStep += m_transform.right * value * m_moveSidewaysSpeed * Time.deltaTime;
+		m_lastMovementStep = m_movementStep;
 	}
 
 	// --------------------------------------------------------------------------------
 
 	public void Rotate(float value)
 	{
-		m_rotationStep += value * m_rotateSpeed * Time.deltaTime;
+		m_rotationStep += value * m_rotationSpeed * Time.deltaTime;
+		m_lastRotationStep = m_rotationStep;
 	}
 
 	// --------------------------------------------------------------------------------
