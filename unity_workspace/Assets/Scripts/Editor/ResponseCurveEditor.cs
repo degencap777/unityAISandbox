e7a30c5f450a89d9065dd6dcde10c297b7860779
libraiger <misssilverbeauty0927@outlook.com>
2018-01-14 19:17:01 +0000

    Added Response curve and custom inspector

---
+++ unity_workspace/Assets/Scripts/Editor/ResponseCurveEditor.cs
@@ -0,0 +1,99 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(ResponseCurve))]
+public class ResponseCurveEditor : Editor
+{
+
+	private ResponseCurve m_responseCurve = null;
+
+	private int m_removeIndex = -1;
+	private int m_shiftUpIndex = -1;
+	private int m_shiftDownIndex = -1;
+	
+	// --------------------------------------------------------------------------------
+
+	public void OnEnable()
+	{
+		m_responseCurve = target as ResponseCurve;
+	}
+
+	// --------------------------------------------------------------------------------
+
+	public override void OnInspectorGUI()
+	{
+		serializedObject.Update();
+
+		m_removeIndex = -1;
+		m_shiftUpIndex = -1;
+		m_shiftDownIndex = -1;
+
+		if (m_responseCurve != null)
+		{
+			EditorGUILayout.LabelField("Edges");
+			for (int i = 0; i < m_responseCurve.m_edges.Count; ++i)
+			{
+				EditorGUILayout.BeginHorizontal();
+
+				m_responseCurve.m_edges[i] = EditorGUILayout.FloatField(m_responseCurve.m_edges[i]);
+
+				// remove element
+				if (GUILayout.Button("Remove"))
+				{
+					m_removeIndex = i;
+				}
+
+				// shift up
+				EditorGUI.BeginDisabledGroup(i == 0);
+				if (GUILayout.Button("Move up"))
+				{
+					m_shiftUpIndex = i;
+				}
+				EditorGUI.EndDisabledGroup();
+
+				// shift down
+				EditorGUI.BeginDisabledGroup(i == (m_responseCurve.m_edges.Count - 1));
+				if (GUILayout.Button("Move down"))
+				{
+					m_shiftDownIndex = i;
+				}
+				EditorGUI.EndDisabledGroup();
+
+				EditorGUILayout.EndHorizontal();
+			}
+
+			// modify list if any buttons pressed (after iterating list)
+			if (m_removeIndex > -1)
+			{
+				m_responseCurve.m_edges.RemoveAt(m_removeIndex);
+			}
+			else if (m_shiftUpIndex > -1)
+			{
+				float temp = m_responseCurve.m_edges[m_shiftUpIndex];
+				m_responseCurve.m_edges[m_shiftUpIndex] = m_responseCurve.m_edges[m_shiftUpIndex - 1];
+				m_responseCurve.m_edges[m_shiftUpIndex - 1] = temp;
+			}
+			else if (m_shiftDownIndex > -1)
+			{
+				float temp = m_responseCurve.m_edges[m_shiftDownIndex];
+				m_responseCurve.m_edges[m_shiftDownIndex] = m_responseCurve.m_edges[m_shiftDownIndex + 1];
+				m_responseCurve.m_edges[m_shiftDownIndex + 1] = temp;
+			}
+
+			// add edge
+			if (GUILayout.Button("+"))
+			{
+				m_responseCurve.m_edges.Add(0.0f);
+			}
+
+			EditorGUILayout.Space();
+			EditorGUILayout.LabelField("Curve");
+
+			// #SteveD >>> draw curve
+
+		}
+
+		serializedObject.ApplyModifiedProperties();
+	}
+
+}
---
+++ unity_workspace/Assets/Scripts/Utility/ResponseCurve.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ResponseCurve : MonoBehaviour
+{
+
+	public List<float> m_edges = new List<float>();
+
+	// --------------------------------------------------------------------------------
+
+	public float GetValue(float position)
+	{
+		float result = 0.0f;
+
+		if (m_edges.Count == 0)
+		{
+			return 0.0f;
+		}
+		else if (m_edges.Count == 1)
+		{
+			return m_edges[0];
+		}
+
+		// #SteveD >>> find bucket edges
+
+		// #SteveD >>> find value between bucket's edges
+
+		return result;
+	}
+	
+}
