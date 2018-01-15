93d48b6613b426a4330217da44509551b34e4d8b
libraiger <misssilverbeauty0927@outlook.com>
2018-01-15 16:00:21 +0000

    Response curve editor WIP

--- unity_workspace/Assets/Scripts/Editor/ResponseCurveEditor.cs
+++ unity_workspace/Assets/Scripts/Editor/ResponseCurveEditor.cs
@@ -6,6 +6,7 @@ public class ResponseCurveEditor : Editor
 {
 
 	private ResponseCurve m_responseCurve = null;
+	private Material m_material = null;
 
 	private int m_removeIndex = -1;
 	private int m_shiftUpIndex = -1;
@@ -16,6 +17,7 @@ public class ResponseCurveEditor : Editor
 	public void OnEnable()
 	{
 		m_responseCurve = target as ResponseCurve;
+		m_material = new Material(Shader.Find("Hidden/Internal-Colored"));
 	}
 
 	// --------------------------------------------------------------------------------
@@ -89,8 +91,52 @@ public class ResponseCurveEditor : Editor
 			EditorGUILayout.Space();
 			EditorGUILayout.LabelField("Curve");
 
-			// #SteveD >>> draw curve
+			// curve container rectangle
+			GUILayout.BeginHorizontal(EditorStyles.helpBox);
+			Rect layoutRectangle = GUILayoutUtility.GetRect(100.0f, 140.0f);
 
+			if (Event.current.type == EventType.Repaint)
+			{
+				// begin
+				GUI.BeginClip(layoutRectangle);
+				GL.PushMatrix();
+				GL.Clear(true, false, Color.black);
+				m_material.SetPass(0);
+
+				// background
+				GL.Begin(GL.QUADS);
+				GL.Color(Color.black);
+				GL.Vertex3(0, 0, 0);
+				GL.Vertex3(layoutRectangle.width, 0, 0);
+				GL.Vertex3(layoutRectangle.width, layoutRectangle.height, 0);
+				GL.Vertex3(0, layoutRectangle.height, 0);
+				GL.End();
+
+				// start drawing lines
+				GL.Begin(GL.LINES);
+				
+				// #SteveD >>> cache inner rect values
+
+				// x axis
+				// #SteveD >>> fix this
+				GL.Vertex3(layoutRectangle.x, layoutRectangle.y - layoutRectangle.height, 0);
+				GL.Vertex3(layoutRectangle.x + (layoutRectangle.width - 50), layoutRectangle.y - layoutRectangle.height, 0);
+
+				// y axis
+				// ...
+				
+				// #SteveD >>> todo >>> draw graph
+				// - if 0 edges, draw solid line at 0.0f
+				// - if 1 edge, draw solid line at this edge value
+				// - draw lines connecting edges
+
+				// end drawing lines
+				GL.End();
+
+				// end
+				GL.PopMatrix();
+				GUI.EndClip();
+			}
 		}
 
 		serializedObject.ApplyModifiedProperties();
