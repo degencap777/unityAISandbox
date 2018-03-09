bc2fcd88b1a77912adb03b9f803c16afb7b004cb
libraiger <misssilverbeauty0927@outlook.com>
2018-03-09 23:00:20 +0000

    Notes

--- unity_workspace/Assets/Scripts/AI/Rooms/RoomManager.cs
+++ unity_workspace/Assets/Scripts/AI/Rooms/RoomManager.cs
@@ -6,6 +6,9 @@ class RoomManager : SingletonMonoBehaviour<RoomManager>
 
 	// #SteveD	>>> custom editor showing all rooms & their inhabitants
 
+	// #SteveD	>>> links between rooms at link location so we can estimate 
+	//				distance between links/rooms
+
 	[SerializeField, HideInInspector]
 	private List<Room> m_rooms = new List<Room>();
 
