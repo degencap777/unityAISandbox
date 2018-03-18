741036e36e1862e9b4b91b4d76639b23ff02226c
libraiger <misssilverbeauty0927@outlook.com>
2018-03-18 11:21:59 +0000

    Allegiance concept

---
+++ unity_workspace/Assets/Scripts/Allegiance/Allegiance.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class Allegiance : IComparable<Allegiance>
+{
+
+	[SerializeField]
+	private string m_name = string.Empty;
+	public string Name { get { return m_name; } }
+
+	[SerializeField]
+	private Color m_colour = new Color(1.0f, 0.33f, 0.66f, 1.0f);
+	public Color Colour { get { return m_colour; } }
+
+	// --------------------------------------------------------------------------------
+
+	private int m_id = -1;
+	public int Id { get { return m_id; } }
+
+	// --------------------------------------------------------------------------------
+
+	public Allegiance(int id)
+	{
+		m_id = id;
+	}
+
+	// --------------------------------------------------------------------------------
+
+	public int CompareTo(Allegiance other)
+	{
+		return Mathf.Clamp(other.m_id - m_id, -1, 1);
+	}
+
+}
---
+++ unity_workspace/Assets/Scripts/Allegiance/AllegianceManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[ExecuteInEditMode]
+public class AllegianceManager : MonoBehaviour
+{
+
+	private static int m_nextUid = 0;
+
+	public static int GetNextUid()
+	{
+		return m_nextUid++;
+	}
+
+	// --------------------------------------------------------------------------------
+
+	[SerializeField]
+	private List<Allegiance> m_allegiances = new List<Allegiance>();
+
+	// --------------------------------------------------------------------------------
+
+	// #SteveD	>>> editor functionality - add allegiance (call Allegiance constructor using GetNextUid())
+	//			>>> custom editor with Add allegiance button at bottom of list, remove allegiance button next to each allegiance
+	//			>>> property drawer for allegiance (one row with background in allegiance colour, name in textbox, change colour button, remove button)
+
+}
